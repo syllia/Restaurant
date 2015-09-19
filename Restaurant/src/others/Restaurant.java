c68853aef6a2939ade157c244a84532f5ae50a3e
syllia <syllia92@gmail.com>
2015-09-19 17:39:34 -0400

    corrections restauration

--- Restaurant/src/others/Restaurant.java
+++ Restaurant/src/others/Restaurant.java
@@ -1,4 +1,5 @@
 package others;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import Reservation.*;
@@ -17,9 +18,123 @@ public class Restaurant /*extends Member*/ {
 	private String descriptionImagesPath;
 	private int capacity;
 	private int numberOfSeatRemaining;
-	private Comment[] comments;
+	private ArrayList<Comment> comments;
 	private Appreciation appreciation;
 	private Service service;
+	
+	
+
+	public Restaurant(String restaurantLogoPath, String restaurantName,
+			String descriptionImagesPath, int capacity,
+			int numberOfSeatRemaining, Comment comments,
+			Appreciation appreciation, Service service) {
+		super();
+		this.restaurantLogoPath = restaurantLogoPath;
+		this.restaurantName = restaurantName;
+		this.descriptionImagesPath = descriptionImagesPath;
+		this.capacity = capacity;
+		this.numberOfSeatRemaining = numberOfSeatRemaining;
+		this.comments.set(0,comments);
+		this.appreciation = appreciation;
+		this.service = service;
+	}
+	
+	
+
+	public String getRestaurantLogoPath() {
+		return restaurantLogoPath;
+	}
+
+
+
+	public void setRestaurantLogoPath(String restaurantLogoPath) {
+		this.restaurantLogoPath = restaurantLogoPath;
+	}
+
+
+
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+
+
+
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+
+
+
+	public String getDescriptionImagesPath() {
+		return descriptionImagesPath;
+	}
+
+
+
+	public void setDescriptionImagesPath(String descriptionImagesPath) {
+		this.descriptionImagesPath = descriptionImagesPath;
+	}
+
+
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+
+
+	public int getNumberOfSeatRemaining() {
+		return numberOfSeatRemaining;
+	}
+
+
+
+	public void setNumberOfSeatRemaining(int numberOfSeatRemaining) {
+		this.numberOfSeatRemaining = numberOfSeatRemaining;
+	}
+
+
+
+	public Comment getComments(int index) {
+		return comments.get(index);
+	}
+
+/*
+	public void setComments(Comment[] comments) {
+		this.comments = comments;
+	}
+*/
+
+
+	public Appreciation getAppreciation() {
+		return appreciation;
+	}
+
+
+
+	public void setAppreciation(Appreciation appreciation) {
+		this.appreciation = appreciation;
+	}
+
+
+
+	public Service getService() {
+		return service;
+	}
+
+
+
+	public void setService(Service service) {
+		this.service = service;
+	}
+
+
 
 	/**
 	 * 
