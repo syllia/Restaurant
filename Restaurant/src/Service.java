0f0a2d2c730ddf1e16f28cab44f7ec8fc09f3616
syllia <syllia92@gmail.com>
2015-09-19 12:04:25 -0400

    Ajout service

--- Restaurant/src/Service.java
+++ Restaurant/src/Service.java
@@ -6,6 +6,40 @@ public class Service {
 	private boolean delivery_allowed;
 	private boolean order_and_pickup_allowed;
 	
+	public Service(boolean reservation_allowed, boolean delivery_allowed,
+			boolean order_and_pickup_allowed) {
+		super();
+		this.reservation_allowed = reservation_allowed;
+		this.delivery_allowed = delivery_allowed;
+		this.order_and_pickup_allowed = order_and_pickup_allowed;
+	}
+
+	public boolean isReservation_allowed() {
+		return reservation_allowed;
+	}
+
+	public void setReservation_allowed(boolean reservation_allowed) {
+		this.reservation_allowed = reservation_allowed;
+	}
+
+	public boolean isDelivery_allowed() {
+		return delivery_allowed;
+	}
+
+	public void setDelivery_allowed(boolean delivery_allowed) {
+		this.delivery_allowed = delivery_allowed;
+	}
+
+	public boolean isOrder_and_pickup_allowed() {
+		return order_and_pickup_allowed;
+	}
+
+	public void setOrder_and_pickup_allowed(boolean order_and_pickup_allowed) {
+		this.order_and_pickup_allowed = order_and_pickup_allowed;
+	}
+	
+	
+	
 	
 
 }
